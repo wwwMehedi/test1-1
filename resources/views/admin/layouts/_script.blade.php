043f88bba24e3031f8f282a591ebf0b5f17cf723
unknown <you@example.com>
2018-10-25 20:17:33 +0600

    init

---
+++ resources/views/admin/layouts/_script.blade.php
@@ -0,0 +1,15 @@
+ <script src="/admin_assets/vendor/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="/admin_assets/vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Metis Menu Plugin JavaScript -->
+    <script src="/admin_assets/vendor/metisMenu/metisMenu.min.js"></script>
+
+    <!-- Morris Charts JavaScript -->
+    <script src="/admin_assets/vendor/raphael/raphael.min.js"></script>
+    <script src="/admin_assets/vendor/morrisjs/morris.min.js"></script>
+    <script src="/admin_assets/data/morris-data.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="/admin_assets/dist/js/sb-admin-2.js"></script>
---
+++ resources/views/admin/layouts/app.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+@include('admin.layouts._head')
+
+<body>
+
+    <div id="wrapper">
+
+    	        <!-- Navigation -->
+        <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.html">SB Admin v2.0</a>
+            </div>
+
+			@include('admin.layouts._header')
+           @include('admin.layouts._sidebar')
+            <!-- /.navbar-static-side -->
+        </nav>
+
+
+
+        <div id="page-wrapper">
+            @yield('AdminPageContent')
+        </div>
+        <!-- /#page-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+
+    <!-- jQuery -->
+   @include('admin.layouts._script')
+
+</body>
+
+</html>
